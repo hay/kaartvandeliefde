eee46c5a879ba2622193ff8d5651cce25a7477eb
Hay Kranen <huskyr@gmail.com>
2015-01-19 15:44:24 +0100

    Made themes configurable

--- web/inc/data.php
+++ web/inc/data.php
@@ -32,4 +32,58 @@ class Data {
             "values" => ["Katholiek", "Protestants", "Joods", "Islamitisch", "Overig", "Niet gelovig"]
         ]
     ];
+
+    public static $themes = [
+        "angst" => [
+            "category" => "Angst in kaart",
+            "title" => "Angst in Nederland",
+            "charts" => [
+                [
+                    "category" => "Bindingsangst",
+                    "single" => "Heeft %s last van bindingsangst?",
+                    "double" => "Is bindingsangst in %s groter dan in %s?"
+                ],
+                [
+                    "category" => "Angst om alleen te blijven",
+                    "single" => "Vindt %s het erg om alleen te blijven?",
+                    "double" => "Vindt %s het erger om alleen te blijven dan %s?"
+                ],
+                [
+                    "category" => "Liever ontevreden dan alleen",
+                    "single" => "Is %s liever ontevreden dan vrijgezel?",
+                    "double" => "Is %s liever ontevreden dan vrijgezel, dan in %s?"
+                ],
+                [
+                    "category" => "Date vinden is gemakkelijk",
+                    "single" => "Vindt %s het makkelijk om een date te vinden?",
+                    "double" => "Vindt men in %s het makkelijker om een date te vinden dan in %s?"
+                ],
+                [
+                    "category" => "Relatie vinden is gemakkelijk",
+                    "single" => "Vindt %s het makkelijk om een relatie te vinden?",
+                    "double" => "Vindt men in %s het makkelijker om een date te vinden dan in %s?"
+                ],
+                [
+                    "category" => "Partner wordt verliefd op ander",
+                    "single" => "Is %s bang dat hun partner verliefd wordt op een ander?",
+                    "double" => "Is men in %s banger dat hun partner verliefd wordt op een ander dan in %s?",
+                ],
+                [
+                    "category" => "Verlatingsangst",
+                    "single" => "Wordt %s wanhopig wanneer hun partner hen verlaat?",
+                    "double" => "Wordt men wanhopiger in %s dan in %s wanneer hun partner hen verlaat?"
+                ],
+                [
+                    "category" => "Verlatingsangst II",
+                    "single" => "Is %s bang om na een relatie alleen te blijven?",
+                    "double" => "Is %s banger om na een relatie alleen te blijven dan in %s?"
+                ],
+                [
+                    "category" => "In relatie door angst alleen te zijn",
+                    "single" => "Houden relaties in %s stand uit angst om alleen te zijn?",
+                    "double" => "Houdt %s de relatie vaker in stand uit angst alleen te zijn dan in %s?"
+                ]
+            ]
+        ]
+    ];
 }
--- web/templates/theme_angst.php
+++ web/templates/theme_angst.php
@@ -1,13 +1,28 @@
-<div class="theme_angst container_page">
-    <div class="contentHeader angst" data-category="Angst in kaart">
+<?php foreach (Data::$themes as $themeId => $theme): ?>
+<div class="theme_<?= $themeId; ?> container_page">
+    <div class="contentHeader <?= $themeId; ?>" data-category="<?= $theme['category']; ?>">
         <div class="container_content">
-            <h1>Angst in Nederland</h1>
+            <h1><?= $theme['title']; ?></h1>
         </div>
     </div>
-    <div class="contentBlock"  data-category="Bindingsangst">
+
+    <?php foreach ($theme['charts'] as $chart) : ?>
+    <div class="contentBlock"
+         data-category="<?= $chart['category'] ?>"
+     >
         <div class="container_content">
-            <span class="singleQuote">Heeft <span class="gemeente_name1"></span> last van bindingsangst?</span>
-            <span class="doubleQuote">Is bindingsangst in <span class="gemeente_name1"></span> groter dan in <span class="gemeente_name2"></span>?</span>
+            <span class="singleQuote">
+                <?php printf($chart['single'], '<span class="gemeente_name1"></span>'); ?>
+            </span>
+
+            <span class="doubleQuote">
+                <?php printf(
+                    $chart['double'],
+                    '<span class="gemeente_name1"></span>',
+                    '<span class="gemeente_name2"></span>'
+                ); ?>
+            </span>
+
             <div class="chartcontainer">
                 <table class="chart_table">
                     <tr>
@@ -24,164 +39,6 @@
             </div>
         </div>
     </div>
-    <div class="contentBlock"  data-category="Angst om alleen te blijven">
-        <div class="container_content">
-            <span class="singleQuote">Vindt <span class="gemeente_name1"></span> het erg om alleen te blijven?</span>
-            <span class="doubleQuote">Vindt <span class="gemeente_name1"></span> het erger om alleen te blijven dan <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Liever ontevreden dan alleen">
-        <div class="container_content">
-            <span class="singleQuote">Is <span class="gemeente_name1"></span> liever ontevreden dan vrijgezel?</span>
-            <span class="doubleQuote">Is <span class="gemeente_name1"></span> liever ontevreden dan vrijgezel, dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Date vinden is gemakkelijk">
-        <div class="container_content">
-            <span class="singleQuote">Vindt <span class="gemeente_name1"></span> het makkelijk om een date te vinden?</span>
-            <span class="doubleQuote">Vindt men in <span class="gemeente_name1"></span> het makkelijker om een date te vinden dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Relatie vinden is gemakkelijk">
-        <div class="container_content">
-            <span class="singleQuote">Vindt <span class="gemeente_name1"></span> het makkelijk om een relatie te vinden?</span>
-            <span class="doubleQuote">Vindt men in <span class="gemeente_name1"></span> het makkelijker om een date te vinden dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Partner wordt verliefd op ander">
-        <div class="container_content">
-            <span class="singleQuote">Is <span class="gemeente_name1"></span> bang dat hun partner verliefd wordt op een ander?</span>
-            <span class="doubleQuote">Is men in <span class="gemeente_name1"></span> banger dat hun partner verliefd wordt op een ander dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Verlatingsangst">
-        <div class="container_content">
-            <span class="singleQuote">Wordt <span class="gemeente_name1"></span> wanhopig wanneer hun partner hen verlaat?</span>
-            <span class="doubleQuote">Wordt men wanhopiger in <span class="gemeente_name1"></span> dan in <span class="gemeente_name2"></span> wanneer hun partner hen verlaat?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="Verlatingsangst II">
-        <div class="container_content">
-            <span class="singleQuote">Is <span class="gemeente_name1"></span> bang om na een relatie alleen te blijven?</span>
-            <span class="doubleQuote">Is <span class="gemeente_name1"></span> banger om na een relatie alleen te blijven dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="contentBlock"  data-category="In relatie door angst alleen te zijn">
-        <div class="container_content">
-            <span class="singleQuote">Houden relaties in <span class="gemeente_name1"></span> stand uit angst om alleen te zijn?</span>
-            <span class="doubleQuote">Houdt <span class="gemeente_name1"></span> de relatie vaker in stand uit angst alleen te zijn dan in <span class="gemeente_name2"></span>?</span>
-            <div class="chartcontainer">
-                <table class="chart_table">
-                    <tr>
-                        <td class = "chart_left">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                        <td class = "chart_right">
-                            <div class="chart_gemeenteName"></div>
-                            <div class="chart_chart"></div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
+    <?php endforeach; ?>
 </div>
+<?php endforeach; ?>
